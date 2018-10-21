e53e9588d9197f557d077152443bedce86a41c62
ClearpointSoftware <john.elliott@ClearpointSoftware.com>
2018-10-21 10:01:23 -0700

    2.1.0.6 final

--- IseThemeManager.psm1
+++ IseThemeManager.psm1
@@ -25,15 +25,15 @@ Function Set-ActiveTheme {
 Function Convert-ARGB {
     Param ([int]$indx)
     ($psxml.StorableColorTheme.Values.Color[$indx].A,
-        $psxml.StorableColorTheme.Values.Color[$indx].R,
-        $psxml.StorableColorTheme.Values.Color[$indx].G,
-        $psxml.StorableColorTheme.Values.Color[$indx].B) | % {
-            $hex = [System.Convert]::ToString($_,16)
-            if ($hex.Length -eq 1) {
-                $hex = "0$hex"
-            }
-            $hexcolor = $hexcolor + $hex
-        }
+     $psxml.StorableColorTheme.Values.Color[$indx].R,
+     $psxml.StorableColorTheme.Values.Color[$indx].G,
+     $psxml.StorableColorTheme.Values.Color[$indx].B) | % {
+         $hex = [System.Convert]::ToString($_,16)
+         if ($hex.Length -eq 1) {
+             $hex = "0$hex"
+         }
+         $hexcolor = $hexcolor + $hex
+    }
     Return "#$hexcolor"
 }
 
@@ -44,10 +44,10 @@ Function Get-ScriptVersion {
         [Parameter(ParameterSetName='BaseName')][switch]$BaseName,
         [Parameter(ParameterSetName='Number')][switch]$Number
     )
-    $rgx = '(?<version>\d+$)'
+    $rgx = '(?<version>\d+$)' # $Matches returns 0 if no version digit
     $ScriptName =  [System.IO.Path]::GetFileNameWithoutExtension($psISE.CurrentFile.FullPath)
     $result = $ScriptName -match $rgx  
-    $ScriptVersion = ($Matches.version) # 0 if not versioned
+    $ScriptVersion = ($Matches.version) 
     switch -Regex ($PSCmdlet.ParameterSetName) {
         'DisplayName' {
             $var = $ScriptName
@@ -56,7 +56,7 @@ Function Get-ScriptVersion {
             $var = $ScriptName -replace($ScriptVersion,'')
         }
         'Number' {
-            $var = [long]$ScriptVersion
+            $var = [int]$ScriptVersion
         }
     }
     Return $var
@@ -87,6 +87,7 @@ Function Load-IseTheme {
     Write-Host "IseTheme: $($psxml.ChildNodes.Name.GetValue(1))"
 }
 
+#TODO: print path and/or prompt to set path in .config for revised/forked files
 Function Get-IseVersion {
     [CmdletBinding(DefaultParameterSetName='Path')]
     Param (
@@ -105,14 +106,14 @@ Function Get-IseVersion {
                 $newfilecode = $psISE.CurrentFile.Editor.Text
             }
             elseif ($_ -match 'Fork') {
-                $newfilename = "$(Get-ScriptVersion -DisplayName)F$([string](Get-Random -Minimum 111 -Maximum 999))" + 'F_0.ps1'
+                $newfilename = "$(Get-ScriptVersion -DisplayName)F_0" + '.ps1'
                 $newfilecode = $psISE.CurrentFile.Editor.SelectedText
             }
             if ($newfilecode.length -gt 0) {
                 $newfile = $psISE.CurrentPowerShellTab.Files.Add()
                 $newfile.Editor.Text = $newfilecode
                 $newfile.Editor.SetCaretPosition(1,1)
-                $newfilepath = Join-Path $(Split-Path $psISE.CurrentFile.FullPath) $newfilename -ErrorAction SilentlyContinue
+                $newfilepath = Join-Path $(Split-Path $psISE.CurrentFile.FullPath) $newfilename
                 if ([System.IO.File]::Exists($newfilepath)) {
                     Write-Host "$newfilename already esists!" -ForegroundColor Magenta
                 }
@@ -121,7 +122,7 @@ Function Get-IseVersion {
                 }
             }
             else {
-                Write-Host 'Nothing to rev or fork!' -ForegroundColor Magenta
+                Write-Host 'Select the code you wish to fork.' -ForegroundColor Green
             }
         }
         default { }
@@ -151,6 +152,7 @@ Function Get-IseTheme {
     if ($ThemeFiles.Count -gt 0) {
         switch ($PSCmdlet.ParameterSetName) {
             List {
+                $psISE.CurrentPowerShellTab.ConsolePane.Clear()
                 $ThemeFiles | Sort-Object -Property Name | % {
                     if (($_.Name -replace $filetype,'') -eq $xmlcfg.configuration.appSettings.ActiveThemeName.value) {
                         Write-Host "$($xmlcfg.configuration.appSettings.ActiveThemeName.value) *"
